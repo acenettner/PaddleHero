79c1ab04d52c869790cf6f174de503d48fbd9485
neetnectar <124236291+acenettner@users.noreply.github.com>
2024-07-26 20:50:57 -0700

    Player movement done

--- Game/paddle_hero.py
+++ Game/paddle_hero.py
@@ -4,24 +4,34 @@ pygame.init()
 # clock
 clock = pygame.time.Clock()
 # Create Canvas
-canvas = pygame.display.set_mode((400, 400))
+cv_width = 250
+cv_height = 400
+canvas = pygame.display.set_mode((cv_width, cv_height))
 
 # Title
 pygame.display.set_caption("PADDLE HERO")
 running = True
 
+scale = 16
 bg_color = (85, 130, 97)
 block_img = pygame.image.load("block.png").convert()
-paddle = pygame.transform.scale(block_img, (16, 16))
+ball = pygame.transform.scale(block_img, (scale, scale))
+
+class Paddle:
+    def __init__(self, position, limits):
+        self.sprite = pygame.transform.scale(block_img, (scale * 5, scale))
+        self.position = position
+        self.limits = limits
+        
 move_right = False
 move_left = False
-pos_x = 0
-pos_y = 0
-pos = (pos_x, pos_y)
+
+paddle = Paddle(pygame.math.Vector2(cv_width / 2, cv_height - scale), (0, cv_width - (scale * 5)))
 
 while running:
     canvas.fill(bg_color)
-    canvas.blit(paddle, pos)
+    canvas.blit(paddle.sprite, paddle.position)
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -35,11 +45,16 @@ while running:
                 move_right = False
             if event.key == pygame.K_LEFT:
                 move_left = False
+                
+    if paddle.position.x > paddle.limits[1]:
+        move_right = False
+    if paddle.position.x < paddle.limits[0]:
+        move_left = False
+                
     if move_right:
-        pos_x += 3
+        paddle.position[0] += 3
     if move_left:
-        pos_x -= 3
-    pos = (pos_x, pos_y)
+        paddle.position[0] -= 3
     # Use pygame.display.update for specific portions of screen to change
     pygame.display.flip()
     # run at 60 fps
